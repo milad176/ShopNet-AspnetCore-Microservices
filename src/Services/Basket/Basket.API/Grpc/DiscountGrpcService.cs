fb451d8f3449d3a76d055802f4bcfdd554e716cf
Milad Kardgar <40167363+milad176@users.noreply.github.com>
2023-12-07 22:05:13 +0100

    Consuming Discount Grpc Service From Basket Microservice when addin card part2

---
+++ src/Services/Basket/Basket.API/Grpc/DiscountGrpcService.cs
@@ -0,0 +1,22 @@
+﻿using Discount.API.Protos;
+
+namespace Basket.API.Grpc
+{
+    public class DiscountGrpcService
+    {
+        private readonly DiscountProtoService.DiscountProtoServiceClient _discountProtoService;
+
+        public DiscountGrpcService(DiscountProtoService.DiscountProtoServiceClient discountProtoService)
+        {
+            _discountProtoService = discountProtoService ?? throw new ArgumentNullException(nameof(discountProtoService));
+        }
+
+        public async Task<CouponModel> GetDiscount(string productName)
+        {
+            var discountRequest = new GetDiscountRequest { ProductName = productName };
+            var discount = await _discountProtoService.GetDiscountAsync(discountRequest);
+
+            return discount;
+        }
+    }
+}
